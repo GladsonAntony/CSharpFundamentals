a257c4f5a197d7249ba5219101a2ed3ea4fd345b
Gladson Antony <Gladson.Antony@neudesic.com>
2023-03-02 16:02:48 +0530

    First Commit

---
+++ CSharpFundamentals/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using System;
+
+namespace CSharpFundamentals
+{
+    class TestProgram01
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is my first time coding in CSharp");
+        }
+    }
+}
+
