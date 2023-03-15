58cc2c28660c52e2173503ccd6d31a5f840561b1
Gladson Antony <GladsonAntony91@gmail.com>
2023-03-15 16:47:17 +0530

    First Selenium Test

---
+++ SeleniumLearning/SeleniumTest01.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace SeleniumLearning
+{
+    public class SeleniumTest01
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void SetupBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new EdgeConfig());
+            driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();            
+        }
+
+        [Test]
+        public void Test01() 
+        {
+            driver.Url = "https://www.google.com/";
+            string pageTitle = driver.Title.ToString();
+            Assert.That (pageTitle, Is.EqualTo("Google"));
+            Thread.Sleep(5000);
+        }
+
+        [TearDown]
+        public void CloseBrowser() 
+        { 
+            driver.Close();
+            driver.Quit();
+        }
+    }
+}
---
+++ SeleniumLearning/UnitTest01.cs
@@ -0,0 +1,31 @@
+namespace SeleniumLearning
+{
+    public class UnitTest01
+    {
+        [SetUp]
+        public void Setup()
+        {
+            TestContext.Progress.WriteLine("This is the Setup Block. It will be executed before each Test Blocks");
+        }
+
+        [Test]
+        public void Test01()
+        {
+            TestContext.Progress.WriteLine("This is the Test01 Block");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test02()
+        {
+            TestContext.Progress.WriteLine("This is the Test02 Block");
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            TestContext.Progress.WriteLine("This is the Tear Down block. It will be executed after each Test Block\n\n");
+        }
+    }
+}
